8449bcf0976fc846227732e4e03630392c4e1ec2
leontaolong <longt8@uw.edu>
2017-04-05 01:09:06 -0700

    Config main() and clean up code

--- apiserver/handlers/summary.go
+++ apiserver/handlers/summary.go
@@ -20,9 +20,8 @@ func getPageSummary(url string) (openGraphProps, error) {
 	//GET the URL
 	resp, err := http.Get(url)
 
-	//if there waspwd an error, report it and exit
+	//if there was an error, return it
 	if err != nil {
-		// log.Fatalf("error fetching URL: %v\n", err)
 		return nil, err
 	}
 
@@ -34,19 +33,25 @@ func getPageSummary(url string) (openGraphProps, error) {
 		return nil, errors.New("Response status was " + resp.Status)
 	}
 
-	//check response content type
+	//check if the response's Content-Type header
+	//starts with "text/html", return an error noting
+	//what the content type was and that you were
+	//expecting HTML
 	ctype := resp.Header.Get("Content-Type")
 	if !strings.HasPrefix(ctype, "text/html") {
 		return nil, errors.New("response content type was " + ctype + " not text/htm")
 	}
 
+	//create a new openGraphProps map instance to hold
+	//the Open Graph properties we find
 	ogProps := make(openGraphProps)
 
 	//create a new tokenizer over the response body
 	tokenizer := html.NewTokenizer(resp.Body)
 
-	//loop until we find the title element and its content
-	//or encounter an error (which includes the end of the file)
+	//tokenize the response body's HTML and extract
+	//any Open Graph properties you find into the map
+	//break if encounter an error (which includes the end of the file
 	for {
 		//get the next token type
 		tokenType := tokenizer.Next()
@@ -80,36 +85,6 @@ func getPageSummary(url string) (openGraphProps, error) {
 			}
 		}
 	}
-	//Get the URL
-	//If there was an error, return it
-
-	//ensure that the response body stream is closed eventually
-	//HINTS: https://gobyexample.com/defer
-	//https://golang.org/pkg/net/http/#Response
-
-	//if the response StatusCode is >= 400
-	//return an error, using the response's .Status
-	//property as the error message
-
-	//if the response's Content-Type header does not
-	//start with "text/html", return an error noting
-	//what the content type was and that you were
-	//expecting HTML
-
-	//create a new openGraphProps map instance to hold
-	//the Open Graph properties you find
-	//(see type definition above)
-
-	//tokenize the response body's HTML and extract
-	//any Open Graph properties you find into the map,
-	//using the Open Graph property name as the key, and the
-	//corresponding content as the value.
-	//strip the openGraphPrefix from the property name before
-	//you add it as a new key, so that the key is just `title`
-	//and not `og:title` (for example).
-
-	//HINTS: https://info344-s17.github.io/tutorials/tokenizing/
-	//https://godoc.org/golang.org/x/net/html
 }
 
 //SummaryHandler fetches the URL in the `url` query string parameter, extracts
@@ -117,49 +92,31 @@ func getPageSummary(url string) (openGraphProps, error) {
 //to the client as a JSON-encoded object.
 func SummaryHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Content-Type", "application/json; charset=utf-8")
+	// allow JavaScript served from other origins to call this API
 	w.Header().Add("Access-Control-Allow-Origin", "*")
 
 	URL := r.FormValue("url")
-
+	//if no `url` parameter was provided, respond with
+	//an http.StatusBadRequest error and return
 	if URL == "" {
 		http.Error(w, "Bad Request, no parameter key 'url' found", http.StatusBadRequest)
+		return
 	}
 
+	//call getPageSummary() passing the requested URL
+	//and holding on to the returned openGraphProps map
 	openGraphMap, err := getPageSummary(URL)
+
+	//if get back an error, respond to the client
+	//with that error and an http.StatusBadRequest code
 	if err != nil && err != io.EOF {
 		http.Error(w, err.Error(), http.StatusBadRequest)
 	}
+
+	//otherwise, respond by writing the openGrahProps
+	//map as a JSON-encoded object
 	encoder := json.NewEncoder(w)
 	if err := encoder.Encode(openGraphMap); err != nil {
 		http.Error(w, "error encoding json: "+err.Error(), http.StatusInternalServerError)
 	}
-
-	// URL = r.URL.Query().Get("url")
-	//Add the following header to the response
-	//   Access-Control-Allow-Origin: *
-	//this will allow JavaScript served from other origins
-	//to call this API
-
-	//get the `url` query string parameter
-	//if you use r.FormValue() it will also handle cases where
-	//the client did POST with `url` as a form field
-	//HINT: https://golang.org/pkg/net/http/#Request.FormValue
-
-	//if no `url` parameter was provided, respond with
-	//an http.StatusBadRequest error and return
-	//HINT: https://golang.org/pkg/net/http/#Error
-
-	//call getPageSummary() passing the requested URL
-	//and holding on to the returned openGraphProps map
-	//(see type definition above)
-
-	//if you get back an error, respond to the client
-	//with that error and an http.StatusBadRequest code
-
-	//otherwise, respond by writing the openGrahProps
-	//map as a JSON-encoded object
-	//add the following headers to the response before
-	//you write the JSON-encoded object:
-	//   Content-Type: application/json; charset=utf-8
-	//this tells the client that you are sending it JSON
 }
--- apiserver/main.go
+++ apiserver/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"challenges-leontaolong/apiserver/handlers"
+	"fmt"
 	"log"
 	"net/http"
 	"os"
@@ -16,24 +17,24 @@ const (
 
 //main is the main entry point for this program
 func main() {
+	//read and use the following environment variables
+	//when initializing and starting the web server
+	// PORT - port number to listen on for HTTP requests (if not set, use defaultPort)
+	// HOST - host address to respond to (if not set, leave empty, which means any host)
 	port := os.Getenv("PORT")
 	host := os.Getenv("HOST")
 
 	if port == "" {
 		port = defaultPort
 	}
-	//read and use the following environment variables
-	//when initializing and starting your web server
-	// PORT - port number to listen on for HTTP requests (if not set, use defaultPort)
-	// HOST - host address to respond to (if not set, leave empty, which means any host)
 
-	http.HandleFunc(apiSummary, handlers.SummaryHandler)
-	//add your handlers.SummaryHandler function as a handler
+	fmt.Printf("server is listening at %s...\n", host+":"+port)
+
+	//add handlers.SummaryHandler function as a handler
 	//for the apiSummary route
-	//HINT: https://golang.org/pkg/net/http/#HandleFunc
+	http.HandleFunc("/v1/summary", handlers.SummaryHandler)
 
 	//start your web server and use log.Fatal() to log
 	//any errors that occur if the server can't start
-	//HINT: https://golang.org/pkg/net/http/#ListenAndServe
-	log.Fatal(http.ListenAndServe(port+":"+host, nil))
+	log.Fatal(http.ListenAndServe(host+":"+port, nil))
 }
