eb601eb3923e7a2c18c3317f42d4d84266cd70ad
leontaolong <longt8@uw.edu>
2017-05-01 17:37:31 -0700

    Set react-routes in index.js

--- webclient/src/index.js
+++ webclient/src/index.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import SignUp from './SignUp';
+import SignIn from './SignIn';
+import Profile from './Profile';
 import './index.css';
 import { IndexRoute, Router, Route, hashHistory } from 'react-router';
 
 ReactDOM.render(
-  <App />,
+  <Router onUpdate={() => window.scrollTo(0, 0)} history={hashHistory}>
+    <Route path="/" component={App}>
+      <IndexRoute component={SignIn} />
+      <Route path="signin" component={SignIn} />
+      <Route path="signup" component={SignUp} />
+      <Route path="profile" component={Profile} />
+    </Route>
+  </Router>,
   document.getElementById('root')
 );
