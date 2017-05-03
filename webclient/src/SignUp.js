fe285b00f72f59ebc4435489cb441cf3d12a7ad1
leontaolong <longt8@uw.edu>
2017-05-03 00:29:50 -0700

    Save changes for server debugging

--- webclient/src/SignUp.js
+++ webclient/src/SignUp.js
@@ -42,12 +42,12 @@ class SignUp extends React.Component {
     //replace `your-domain.com` with your domain name
     var apiURL = "https://api.leontaolong.me/v1/";
 
-    //if our site is being served from localhost,
-    //or the loop-back address, switch the base API URL
-    //to localhost as well
-    // if (window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1") {
-    //     apiURL = "https://localhost:4000/v1/"
-    // };
+    // if our site is being served from localhost,
+    // or the loop-back address, switch the base API URL
+    // to localhost as well
+    if (window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1") {
+        apiURL = "https://localhost:2222/v1/"
+    };
 
     var myHeaders = new Headers();
     myHeaders.append("Content-Type", "application/json");
