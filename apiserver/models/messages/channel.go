58bd96ce7dff1d51212eb69d252617e87f73a0a2
leontaolong <longt8@uw.edu>
2017-05-07 14:40:54 -0700

    Define Channel and NewChannel models

---
+++ apiserver/models/messages/channel.go
@@ -0,0 +1,25 @@
+package channels
+
+import (
+	"challenges-leontaolong/apiserver/models/users"
+	"time"
+)
+
+//Channel represents a channel
+type Channel struct {
+	ID          string         `json:"id" bson:"_id"`
+	Name        string         `json:"name"`
+	Description string         `json:"description"`
+	CreatedAt   time.Time      `json:"createdAt"`
+	CreatorID   users.UserID   `json:"creatorID"`
+	Members     []users.UserID `json:"members"`
+	Private     bool           `json:"private"`
+}
+
+//NewChannel represents a new channel
+type NewChannel struct {
+	Name        string         `json:"name"`
+	Description string         `json:"description"`
+	Members     []users.UserID `json:"members"`
+	Private     bool           `json:"private"`
+}
